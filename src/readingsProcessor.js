6ea1f484a1eafd29da8376cd54062e9cac599745
shawn <shawnbutton@gmail.com>
2023-08-24 10:51:03 -0400

    add test and source for functional refactoring kata

---
+++ src/readingsProcessor.js
@@ -0,0 +1,34 @@
+'use strict'
+
+function processReadings (readings) {
+  const environmental = []
+  const asset = []
+  const vehicle = []
+  for (let i = 0; i < readings.length; i++) {
+    const reading = readings[i]
+
+    // only process if we received data for reading
+    if (reading.data.length > 0 && !reading.inactive) {
+      // convert temperature readings to fahrenheit
+      reading.temperature = reading.temperature * 1.8 + 32
+
+      if (reading.type === 'environmental') {
+        environmental.push(reading)
+      } else if (reading.type === 'asset') {
+        asset.push(reading)
+      } else if (reading.type === 'vehicle') {
+        vehicle.push(reading)
+      }
+    }
+  }
+
+  const grouped = {}
+
+  if (environmental.length > 0) grouped.environmental = environmental
+  if (asset.length > 0) grouped.asset = asset
+  if (vehicle.length > 0) grouped.vehicle = vehicle
+
+  return grouped
+}
+
+module.exports = { processReadings }
---
+++ tests/readingsProcessor.spec.js
@@ -0,0 +1,186 @@
+'use strict'
+/* eslint-env jest */
+
+const { clone } = require('ramda')
+
+const sut = require('../src/readingsProcessor')
+
+describe('process readings characterization test', () => {
+  it('matches expected', () => {
+    const given = [
+      {
+        name: 'no data is ignored',
+        data: [],
+        temperature: 0,
+        type: 'environmental'
+      },
+      {
+        name: 'inactive is ignored',
+        data: [0],
+        inactive: true,
+        temperature: 0,
+        type: 'environmental'
+      },
+      {
+        name: 'environmental is grouped in environmental',
+        data: [0],
+        temperature: 0,
+        type: 'environmental'
+      },
+      {
+        name: 'asset is grouped in asset',
+        data: ['abc'],
+        temperature: -10,
+        type: 'asset'
+      },
+      {
+        name: 'vehicle type is grouped in vehicle',
+        data: ['abc'],
+        temperature: 10,
+        type: 'vehicle'
+      },
+      {
+        name: 'other types are ignored',
+        data: ['abc'],
+        temperature: 10,
+        type: 'other'
+      }
+    ]
+
+    const expected = {
+      environmental: [
+        {
+          name: 'environmental is grouped in environmental',
+          data: [
+            0
+          ],
+          temperature: 32,
+          type: 'environmental'
+        }
+      ],
+      asset: [
+        {
+          name: 'asset is grouped in asset',
+          data: [
+            'abc'
+          ],
+          temperature: 14,
+          type: 'asset'
+        }
+      ],
+      vehicle: [
+        {
+          name: 'vehicle type is grouped in vehicle',
+          data: [
+            'abc'
+          ],
+          temperature: 50,
+          type: 'vehicle'
+        }
+      ]
+    }
+
+    const result = sut.processReadings(given)
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe('process readings', () => {
+  const buildReading = () => {
+    return {
+      data: [0],
+      temperature: 0,
+      type: 'default'
+    }
+  }
+
+  it('should ignore readings with no data', () => {
+    const given = buildReading()
+    given.data = []
+
+    const expected = {}
+
+    expect(sut.processReadings([given])).toEqual(expected)
+  })
+
+  it('should ignore readings that are inactive', () => {
+    const given = buildReading()
+    given.inactive = true
+
+    const expected = {}
+
+    expect(sut.processReadings([given])).toEqual(expected)
+  })
+
+  test('environmental is grouped', () => {
+    const given = buildReading()
+    given.type = 'environmental'
+
+    const expected = {
+      environmental: [
+        {
+          data: [0],
+          temperature: 32,
+          type: 'environmental'
+        }
+      ]
+    }
+
+    expect(sut.processReadings([given])).toEqual(expected)
+  })
+
+  test('asset is grouped', () => {
+    const given = buildReading()
+    given.type = 'asset'
+
+    const expected = {
+      asset: [
+        {
+          data: [0],
+          temperature: 32,
+          type: 'asset'
+        }
+      ]
+    }
+
+    expect(sut.processReadings([given])).toEqual(expected)
+  })
+
+  test('vehicle is grouped', () => {
+    const given = buildReading()
+    given.type = 'vehicle'
+
+    const expected = {
+      vehicle: [
+        {
+          data: [0],
+          temperature: 32,
+          type: 'vehicle'
+        }
+      ]
+    }
+
+    expect(sut.processReadings([given])).toEqual(expected)
+  })
+
+  test('other types are ignored', () => {
+    const given = buildReading()
+    given.type = 'something unknown'
+
+    expect(sut.processReadings([given])).toEqual({})
+  })
+
+  xit('should not mutate readings', () => {
+    const given = [{
+      data: [0],
+      temperature: 0
+    }]
+
+    const identicalToGiven = clone(given)
+
+    sut.processReadings(given)
+
+    expect(given).toEqual(identicalToGiven)
+  })
+})
